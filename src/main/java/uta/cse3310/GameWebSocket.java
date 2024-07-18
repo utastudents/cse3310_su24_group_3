03e2bf14cb16f3638f5f54a86ebea0875482b21c
ravi-ray96 <rkr9684@mavs.uta.edu>
2024-07-18 12:30:58 -0400

    MyBranchUpdates

--- src/main/java/uta/cse3310/GameWebSocket.java
+++ src/main/java/uta/cse3310/GameWebSocket.java
@@ -6,31 +6,31 @@ import org.java_websocket.server.WebSocketServer;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.WebSocket;
 
-public class WebSocketServer extends WebSocketServer {
+public class GameWebSocket extends GameWebSocket {
 
     private int port;
     private WebSocket conn;
 
-    public WebSocketServer(int port) {
+    public GameWebSocket(int port) {
         super(new InetSocketAddress(port));
         this.port = port;
     }
 
     @Override
-    //Websocket session is opening
+    // Websocket session is opening
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         this.conn = conn;
         System.out.println("New connection opened: " + conn.getRemoteSocketAddress());
     }
 
     @Override
-    //Incoming WebSocket messages
+    // Incoming WebSocket messages
     public void onMessage(WebSocket conn, String message) {
         System.out.println("Message received: " + message);
     }
 
     @Override
-    //WebSocket session is closing
+    // WebSocket session is closing
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         System.out.println("Connection closed: " + conn.getRemoteSocketAddress() + ", Reason: " + reason);
     }
@@ -43,9 +43,10 @@ public class WebSocketServer extends WebSocketServer {
     }
 
     public static void main(String[] args) {
-        int port = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "8080")) + 100; //Default port used as of now
-        WebSocketServer server = new WebSocketServer(port);
-        //begin execution of thread
+        int port = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "8080")) + 100; // Default port used as of
+                                                                                              // now
+        GameWebSocket server = new GameWebSocket(port);
+        // begin execution of thread
         server.start();
         System.out.println("WebSocket server started on port: " + port);
     }
