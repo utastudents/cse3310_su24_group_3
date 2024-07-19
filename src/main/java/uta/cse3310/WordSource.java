32a5e6651a28518a412ff5989bf89377faba96f5
Tyler Crouch <tcc3907@mavs.uta.edu>
2024-07-19 13:01:50 -0500

    Implemented class WordSource

---
+++ src/main/java/uta/cse3310/WordSource.java
@@ -0,0 +1,22 @@
+package uta.cse3310;
+
+import java.util.List;
+
+public class WordSource {
+
+    private String FilePath;
+    private List<String> Words;
+    private String ChosenWord;
+
+    public void GetSource(String filePath) {
+
+    }
+
+    public void ReadWordFromFile() {
+
+    }
+
+    public String ChooseRandomWord() {
+        
+    }
+}
