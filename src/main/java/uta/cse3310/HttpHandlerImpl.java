49bf756fc9fc152a1fe85bfab7a50c0cbd89cd96
loganwells <lxw3153@mavs.uta.edu>
2024-07-18 23:50:24 -0500

    Update HttpHandlerImpl.java

--- src/main/java/uta/cse3310/HttpHandlerImpl.java
+++ src/main/java/uta/cse3310/HttpHandlerImpl.java
@@ -1,5 +1,33 @@
 package uta.cse3310;
 
-public class HttpHandlerImpl {
+public class HttpHandlerImpl implements HttpHandler {
 
+    private int httpPort;
+    private int websocketPort;
+
+public HttpHandlerImpl(int httpPort) {
+        this.httpPort = httpPort;
+        this.websocketPort = httpPort + 100; // WebSocket port is HTTP port + 100
+    }
+
+ 
+public void startServer() {
+      //Implement 
 }
+
+/**
+ * Handles client requests to the server.
+ * 
+ */
+
+  public void handle(){
+          //Implement 
+  }
+
+  public void broadcastMessage(String message) {
+        // Implementation  added later
+  }
+}
+
+
+  
