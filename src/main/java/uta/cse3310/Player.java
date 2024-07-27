d042434659fb87ded86d9ed7c690451df43d65ff
Noe Sanchez <noesanchez00000@gmail.com>
2024-07-26 23:14:12 -0500

    added PlayerType and updated the player class
    added PlayerType so that we would abel to tell if the player is 1,2,3,4 in the game, and currently updating the playerTest file

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,60 +1,93 @@
 package uta.cse3310;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
 
     private String name;
     private int score;
-    private Boolean isActive;
+    private int id;
+    private boolean isActive;
+    private static List<Player> playerList = new ArrayList<>();
+    private PlayerType type;
     
-    public Player(){
-    this.name = "";
-    this.score = 0;
-    this.isActive = false;
+    public Player(String name) {
+        if (verifyName(name)) {
+            this.name = name;
+            this.id = playerList.size() + 1;
+            this.isActive = true;
+            this.score = 0;
+            playerList.add(this);
+        } else {
+            this.name = null;
+            this.id = -1;  // invalid id since the name is not valid
+        }
     }
 
-    public Player (String name, int score){
-        this.name = name;
-        this.score = score;
-        this.isActive = true;
+    public boolean verifyName(String playerName) {
+        for (Player player : playerList) {
+            if (playerName.equals(player.getName())) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+    
+    public void setName(String name) {
         this.name = name;
     }
-    public void setScore(int score){
-        this.score =score;
+
+    public int getId() {
+        return id;
     }
-    public int getScore(){
+
+    public void removePlayer(int playerId) {
+        playerList.removeIf(player -> player.id == playerId);
+    }
+
+    public PlayerType getPlayerOrder() {
+        return type;
+    }
+
+    public void setPlayerOrder(PlayerType type) {
+        this.type = type;
+    }
+
+    public int getScore() {
         return score;
     }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
     public void addPoints(int points) {
         this.score += points;
     }
 
-
     public void resetPoints() {
-        this.score =0;
-        
-    }
-    public void updateScore(int points){
-        this.score =points;
+        this.score = 0;
     }
 
-    public Boolean isActivePlayers() {
+    public void updateScore(int points) {
+        this.score = points;
+    }
+
+    public boolean isActivePlayer() {
         return isActive;
     }
 
-    public Boolean setActivePlayer(Boolean isActive) {
-        return isActive;
+    public void setActivePlayer(boolean isActive) {
+        this.isActive = isActive;
     }
 
-    ArrayList<String> buyVowel(char vowel, ArrayList<String> words) {
-        ArrayList<String> updatedWords = new ArrayList<>();
+    public List<String> buyVowel(char vowel, List<String> words) {
+        List<String> updatedWords = new ArrayList<>();
         for (String str : words) {
             if (str.indexOf(vowel) != -1) {
                 updatedWords.add(str.replaceAll(String.valueOf(vowel), ""));
@@ -65,8 +98,8 @@ public class Player {
         return updatedWords;
     }
 
-    ArrayList<String> chooseConsonant(char consonant, ArrayList<String> words) {
-        ArrayList<String> updatedWords = new ArrayList<>();
+    public List<String> chooseConsonant(char consonant, List<String> words) {
+        List<String> updatedWords = new ArrayList<>();
         for (String str : words) {
             if (str.indexOf(consonant) != -1) {
                 updatedWords.add(str.replaceAll(String.valueOf(consonant), ""));
@@ -77,13 +110,12 @@ public class Player {
         return updatedWords;
     }
 
-    boolean solvePuzzle(ArrayList<String> solution, ArrayList<String> puzzle) {
+    public boolean solvePuzzle(List<String> solution, List<String> puzzle) {
         if (solution.equals(puzzle)) {
-            this.addPoints(1000); //Any amount of points, just an example
+            this.addPoints(1000); // Example points
             return true;
         } else {
             return false;
         }
     }
-
 }
---
+++ src/main/java/uta/cse3310/PlayerType.java
@@ -0,0 +1,6 @@
+package uta.cse3310;
+
+public enum PlayerType {
+    // a player can be in any one of these positions
+ ONE,TWO,THREE,FOUR
+}
